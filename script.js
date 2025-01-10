bc06a3879c540a2cb1f7f8b9169a191a6be82cdf
FES001 <firdavs7794948@gmail.com>
2025-01-10 21:10:56 +0500

    Working on it.

---
+++ script.js
@@ -0,0 +1,6 @@
+const inputContainers = document.querySelectorAll('.mortgage-calculator-left-bottom-input-container');
+const radioInputContainers = document.querySelectorAll('.mortgage-calculator-left-bottom-input-radio-container');
+const errorMessages = document.querySelectorAll('.mortgage-calculator-error-message');
+const errorMessageRadioInput = document.querySelectorAll('.mortgage-calculator-error-message-radio');
+const inputs = document.querySelectorAll('.mortgage-calculator-left-bottom input:not(#repaymentTypeInput, #interestOnlyTypeInput)');
+const submitButton = document.getElementById('submitButton');
